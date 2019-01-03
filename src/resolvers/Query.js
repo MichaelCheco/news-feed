4adb556c35c3e9ec348bbf40293f36008c9effdf
Michael Checo <checomichael2@gmail.com>
2019-01-03 18:37:12 -0500

    Query-feed resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -4,6 +4,9 @@ const Query = {
     me: (parent, args, ctx) => {
         const userId = getUserId(ctx)
         return ctx.prisma.user({ id: userId})
+    },
+    feed: (parent, args, ctx) => {
+        return ctx.prisma.posts()
     }
 }
 
