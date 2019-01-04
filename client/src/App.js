1602183965c44b76130840203cec4301c14f9e65
Michael Checo <checomichael2@gmail.com>
2019-01-04 14:45:07 -0500

    Posts component now displays a list of posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Login from './components/Login'
+import Posts from './components/Posts'
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Login />
+        <Posts />
       </div>
     );
   }
---
+++ client/src/components/Posts.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const POSTS_QUERY = gql`
+  query {
+    feed {
+        id
+      title
+      content
+      createdAt
+      author {
+        name
+      }
+    }
+  }
+`;
+
+class Posts extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <Query query={POSTS_QUERY}>
+          {({ loading, error, data }) => {
+            console.log(data, "DATA");
+            if (loading) return <h3>Loading . . .</h3>;
+            if (error) return <h3>Error:`${error}` . . .</h3>;
+            const posts = data.feed;
+            return (
+                <div>
+                    {posts.map(post => <h2 key={post.id}>{post.title}</h2>)}
+                </div>
+            )
+          }}
+        </Query>
+      </div>
+    );
+  }
+}
+
+export default Posts;
