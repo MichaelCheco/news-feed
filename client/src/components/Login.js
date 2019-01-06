7ff0926c4514567380cf5a3fe14ee9954919e377
Michael Checo <checomichael2@gmail.com>
2019-01-06 14:39:26 -0500

    Login component styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,103 @@ import React, { Component } from "react";
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 import { AUTH_TOKEN } from "../constants";
-
+import styled from "styled-components";
+const Container = styled.div`
+  display: grid;
+  overflow: scroll;
+  grid-column: 2 / 7;
+  grid-row: 3 / -1;
+  margin-top: 30px;
+  background: white;
+  border: 1px solid grey;
+  border-radius: 3px;
+`;
+const EmailInput = styled.input`
+  width: 400px;
+  height: 60px;
+  border: 3px solid ${props => props.theme.darkBlue};
+  border-radius: 4px;
+  font-size: 36px;
+  margin-bottom: 10px;
+  :focus {
+    outline: 0;
+  }
+`;
+const PasswordInput = styled.input`
+  width: 400px;
+  height: 60px;
+  border-radius: 4px;
+  border: 3px solid ${props => props.theme.darkBlue};
+  font-size: 36px;
+  margin-bottom: 10px;
+  :focus {
+    outline: 0;
+  }
+`;
+const NameInput = styled.input`
+  width: 400px;
+  height: 60px;
+  border-radius: 4px;
+  border: 3px solid ${props => props.theme.darkBlue};
+  font-size: 36px;
+  margin-bottom: 10px;
+  :focus {
+    outline: 0;
+  }
+`;
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 680px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  height: 100px;
+  margin-top: 10px;
+`;
+const LoginButton = styled.button`
+  width: 135px;
+  height: 45px;
+  padding: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  background: ${props => props.theme.darkBlue};
+  border: 3px solid ${props => props.theme.darkBlue};
+  color: whitesmoke;
+  border-radius: 10px;
+  margin-right: 10px;
+  :focus {
+    outline: 0;
+  }
+  :hover {
+    color: ${props => props.theme.darkBlue};
+    cursor: pointer;
+    background: whitesmoke;
+  }
+`;
+const CreateButton = styled.button`
+  width: 135px;
+  height: 45px;
+  padding: 4px;
+  font-size: 14px;
+  font-weight: bold;
+  background: ${props => props.theme.darkBlue};
+  border: 3px solid ${props => props.theme.darkBlue};
+  color: whitesmoke;
+  border-radius: 10px;
+  margin-right: 10px;
+  :focus {
+    outline: 0;
+  }
+  :hover {
+    color: ${props => props.theme.darkBlue};
+    cursor: pointer;
+    background: whitesmoke;
+  }
+`;
 const SIGNUP_MUTATION = gql`
   mutation signup($email: String!, $password: String!, $name: String!) {
     signup(email: $email, password: $password, name: $name) {
@@ -31,22 +127,39 @@ class Login extends Component {
     this.setState({ [name]: val });
   };
   _confirmData = async data => {
-      const { token } = this.state.login ? data.login : data.signup
-      this._saveUserData(token)
-      this.props.history.push('/')
-
-  }
+    const { token } = this.state.login ? data.login : data.signup;
+    this._saveUserData(token);
+    this.props.history.push("/");
+  };
   _saveUserData = token => {
-    localStorage.setItem(AUTH_TOKEN, token)
-  }
+    localStorage.setItem(AUTH_TOKEN, token);
+  };
 
   render() {
     const { email, name, password, login } = this.state;
     return (
-      <div>
-        <div>
+      <Container>
+        <ButtonsWrapper>
+          <Mutation
+            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
+            variables={{ email, name, password }}
+            onCompleted={data => this._confirmData(data)}
+          >
+            {mutation => (
+              <LoginButton onClick={mutation}>
+                {login ? "Login" : "Create Account"}
+              </LoginButton>
+            )}
+          </Mutation>
+          <CreateButton onClick={() => this.setState({ login: !login })}>
+            {login ? "Create an account" : "Already have an account?"}
+          </CreateButton>
+        </ButtonsWrapper>
+
+        <InputWrapper>
           {!login && (
-            <input
+            <NameInput
+              autoFocus
               type="text"
               name="name"
               value={name}
@@ -54,40 +167,25 @@ class Login extends Component {
               onChange={this.handleChange}
             />
           )}
-          <input
+          <EmailInput
+            autoFocus
+            required
             type="text"
             name="email"
             value={email}
             placeholder="Email"
             onChange={this.handleChange}
           />
-          <input
-            type="text"
+          <PasswordInput
+            required
+            type="password"
             name="password"
             value={password}
             placeholder="Password"
             onChange={this.handleChange}
           />
-        </div>
-        <div>
-          <Mutation
-            mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
-            variables={{ email, name, password }}
-            onCompleted={data => this._confirmData(data)}
-          >
-            {mutation => (
-              <button onClick={mutation}>
-                {login ? "Login" : "Create Account"}
-              </button>
-            )}
-          </Mutation>
-        </div>
-        <div>
-          <button onClick={() => this.setState({ login: !login })}>
-            {login ? "Create an account" : "Already have an account?"}
-          </button>
-        </div>
-      </div>
+        </InputWrapper>
+      </Container>
     );
   }
 }
