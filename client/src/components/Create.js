e14e193d99977d313377d428004b960b1091466e
Michael Checo <checomichael2@gmail.com>
2019-01-06 03:23:47 -0500

    Styling for Create Component

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,7 +1,52 @@
-import React, { Component } from "react";
+import React, { Component, PureComponent } from "react";
 import { withApollo, Mutation } from "react-apollo";
+import { withRouter } from 'react-router'
 import gql from "graphql-tag";
-
+import styled from "styled-components";
+const Container = styled.div`
+  display: grid;
+  grid-column: 2 / 7;
+  grid-row: 3 / -1;
+  margin-top: 30px;
+  background: white;
+  border: 1px solid grey;
+  border-radius: 3px;
+`;
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+const Textarea = styled.textarea`
+  border: 1px solid lightgrey;
+  margin-top: 20px;
+  width: 90%;
+  font-size: 20px;
+  margin-left: 15px;
+  :focus {
+    outline: 0;
+  }
+`;
+const Button = styled.button`
+  margin-top: 10px;
+  margin-left: 18px;
+  border-radius: 5px;
+  font-size: 24px;
+  width: 200px;
+  height: 50px;
+  background: ${props => props.theme.mediumBlue};
+  color: white;
+  padding: 5px;
+  cursor: pointer;
+  :hover {
+      background: #03669997;
+  }
+  :focus {
+    outline: 0;
+  }
+`;
+const H1 = styled.h1`
+  margin-left: 17px;
+`;
 const CREATE_POST_MUTATION = gql`
   mutation CreatePostMutation($title: String!, $content: String!) {
     createPost(title: $title, content: $content) {
@@ -15,8 +60,17 @@ const CREATE_POST_MUTATION = gql`
     }
   }
 `;
+const Input = styled.input`
+  width: 90%;
+  margin-left: 15px;
+  height: 50px;
+  font-size: 20px;
+  :focus {
+    outline: 0;
+  }
+`;
 
-class Create extends Component {
+class Create extends PureComponent {
   state = {
     title: "",
     content: ""
@@ -30,34 +84,40 @@ class Create extends Component {
       <Mutation mutation={CREATE_POST_MUTATION}>
         {(createPost, { data, loading, error }) => {
           return (
-            <div>
-                <form onSubmit={async e => {
-                    console.log('CREATE',  CREATE_POST_MUTATION)
-                    e.preventDefault()
-                    const { title, content } = this.state;
-                    await createPost({
-                        variables: { title, content}
-                    })
-                    }}>
-              <h1>Create Post</h1>
-              <input
-                autoFocus
-                name="title"
-                value={this.state.title}
-                placeholder="Title"
-                onChange={this.handleChange}
-              />
-              <textarea
-                cols={50}
-                rows={0}
-                placeholder="Content"
-                name="content"
-                value={this.state.content}
-                onChange={this.handleChange}
-              />
-              <button>Create</button>
+            <Container>
+              <form
+                onSubmit={async e => {
+                  console.log("CREATE", CREATE_POST_MUTATION);
+                  e.preventDefault();
+                  const { title, content } = this.state;
+                  await createPost({
+                    variables: { title, content }
+                  });
+                  this.props.history.push('/')
+                }}
+              >
+                <H1>Create Post</H1>
+                <InputWrapper>
+                  <Input
+                    autoFocus
+                    name="title"
+                    value={this.state.title}
+                    placeholder="Title"
+                    onChange={this.handleChange}
+                  />
+                  <Textarea
+                    cols="10"
+                    rows="20"
+                    required
+                    placeholder="Content"
+                    name="content"
+                    value={this.state.content}
+                    onChange={this.handleChange}
+                  />
+                  <Button>Create</Button>
+                </InputWrapper>
               </form>
-            </div>
+            </Container>
           );
         }}
       </Mutation>
@@ -65,4 +125,4 @@ class Create extends Component {
   }
 }
 
-export default withApollo(Create);
+export default withApollo(withRouter(Create));
