854df1ea26b86d81611ab8b490eb7a5bdb545c20
Michael Checo <checomichael2@gmail.com>
2019-01-08 17:10:02 -0500

    comments resolver on User type

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -2,6 +2,9 @@ const User = {
     posts: ({ id }, args, ctx) => {
       return ctx.prisma.user({ id }).posts()
     },   
+    comments: ({ id }, args, ctx) => {
+      return ctx.prisma.user({ id }).comments()
+    }
   }
   
   module.exports = {
