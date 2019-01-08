c5229812443c9dc8ed5b65a82df7201fa3555d3c
Michael Checo <checomichael2@gmail.com>
2019-01-08 17:11:12 -0500

    comments resolver on Post type

--- src/resolvers/Post.js
+++ src/resolvers/Post.js
@@ -1,9 +1,12 @@
 const Post = {
-    author: ({ id }, args, ctx) => {
-        return ctx.prisma.post({ id }).author()
-    }
-}
+  author: ({ id }, args, ctx) => {
+    return ctx.prisma.post({ id }).author();
+  },
+  comments: ({ id }, args, ctx) => {
+    return ctx.prisma.post({ id }).comments();
+  }
+};
 
 module.exports = {
-    Post
-}
+  Post
+};
