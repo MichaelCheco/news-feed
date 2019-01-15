9558325fbd6db4e6b8ab991b7342d0e3fa107972
Michael Checo <checomichael2@gmail.com>
2019-01-15 14:53:53 -0500

    Aligned Tracks

--- client/src/components/Tracks.js
+++ client/src/components/Tracks.js
@@ -20,25 +20,35 @@ const TRACKS_QUERY = gql`
     }
   }
 `;
-
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+const H2 = styled.h2`
+  font-family: "Thasadith", sans-serif;
+  font-size: 21px;
+  font-weight: bold;
+`;
 class Tracks extends Component {
   state = {};
   render() {
     return (
       <Container>
-        <Query query={TRACKS_QUERY}>
-          {({ data, loading, error }) => {
-            console.log(data, "DATA");
-            if (loading)
-              return <CubeGrid size={90} color={props => props.theme.red} />;
-            if (error) return <h3>Error:`${error}` . . .</h3>;
-            const tracks = data.tracks;
-            console.log(tracks, "tracks");
-            return tracks.map((track, index) => (
-              <h2 key={index}>{track.name}</h2>
-            ));
-          }}
-        </Query>
+        <Wrapper>
+          <Query query={TRACKS_QUERY}>
+            {({ data, loading, error }) => {
+              console.log(data, "DATA");
+              if (loading)
+                return <CubeGrid size={90} color={props => props.theme.red} />;
+              if (error) return <h3>Error:`${error}` . . .</h3>;
+              const tracks = data.tracks;
+              console.log(tracks, "tracks");
+              return tracks.map((track, index) => (
+                  <H2 key={index}>{track.name}</H2>
+              ));
+            }}
+          </Query>
+        </Wrapper>
       </Container>
     );
   }
