18469a7a6df6e3bb77e5c84024211f9b54bd7915
Michael <checomichael2@gmail.com>
2019-02-24 09:42:42 -0500

    positioned read more buttons

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,23 +5,33 @@ import { withRouter } from 'react-router';
 import { timeDifferenceForDate } from '../utils';
 const PostWrapper = styled.div`
 	@media (min-width: 375px) {
-		border: 3px solid yellow;
 	}
 `;
 function trimPost(post) {
 	var string = post;
-	var length = 150;
+	var length = 165;
 	var trimmedString = string.substring(0, length) + '...';
 	return trimmedString;
 }
+const More = styled.div`
+	@media (min-width: 375px) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+`;
 const Button = styled.button`
 	@media (min-width: 375px) {
 		border: 1px solid ${props => props.theme.mediumBlue};
-		width: 85px;
-		height: 35px;
+		width: 80px;
+		height: 30px;
 		border-radius: 5px;
 		margin-left: 0.5rem;
 		background: ${props => props.theme.mediumBlue};
+		/* position: relative;
+		top: 70%;
+		right: 90%; */
+
 		&:hover {
 			cursor: pointer;
 			opacity: 0.7;
@@ -41,12 +51,14 @@ class Post extends Component {
 			<PostWrapper>
 				<div>
 					<h2>{title}</h2>
-					<p>{trimPost(content)}</p>
-					<Link to={`/post/${this.props.post.id}`}>
-						<Button>
-							<a>Read More</a>
-						</Button>
-					</Link>
+					<More>
+						<p>{trimPost(content)}</p>
+						<Link to={`/post/${this.props.post.id}`}>
+							<Button>
+								<a>Read More</a>
+							</Button>
+						</Link>
+					</More>
 					<p>
 						Posted By {this.props.post.author.name}{' '}
 						{timeDifferenceForDate(createdAt)}
