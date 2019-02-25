87ae397b4f849a45c71e767d8dec7836fb45201e
Michael <checomichael2@gmail.com>
2019-02-25 11:57:42 -0500

    updating alignment and button on contact component

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,16 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+const Button = styled.button`
+	@media (min-width: 375px) {
+		border: 1px solid ${props => props.theme.mediumBlue};
+		width: 80px;
+		height: 30px;
+		border-radius: 5px;
+		margin-left: 0.5rem;
+		background: ${props => props.theme.mediumBlue};
+		/* position: relative;
+		top: 70%;
+		right: 90%; */
 
+		&:hover {
+			cursor: pointer;
+			opacity: 0.7;
+		}
+		outline: 0;
+		a {
+			font-size: 1.3rem;
+			white-space: nowrap;
+			color: white;
+		}
+	}
+`;
+const Wrapper = styled.div`
+	text-align: center;
+`;
 const Contact = () => {
 	return (
-		<div>
-			<div>
-				<h1>Questions? Reach Out.</h1>
-				<button>
-					<a href="mailto:contact@lambdaschool.com">Email Us</a>
-				</button>
-			</div>
-		</div>
+		<Wrapper>
+			<h1>Questions? Reach Out.</h1>
+			<Button>
+				<a href="mailto:contact@lambdaschool.com">Email Us</a>
+			</Button>
+		</Wrapper>
 	);
 };
 
