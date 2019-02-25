8fb8194f7cc45c8ab645ffe69b1c9c4754c33f0e
Michael <checomichael2@gmail.com>
2019-02-25 12:14:54 -0500

    styling for search component

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -18,7 +18,63 @@ const FEED_SEARCH_QUERY = gql`
 		}
 	}
 `;
+const SearchWrapper = styled.div`
+	display: flex;
+	width: 100%;
 
+	input {
+		width: 50%;
+		font-size: 1.5rem;
+		:focus {
+			outline: 0;
+		}
+	}
+`;
+const PostButton = styled.button`
+	@media (min-width: 375px) {
+		border: 1px solid ${props => props.theme.mediumBlue};
+		width: 80px;
+		height: 30px;
+		border-radius: 5px;
+		margin-left: 0.5rem;
+		background: ${props => props.theme.mediumBlue};
+		&:hover {
+			cursor: pointer;
+			opacity: 0.7;
+		}
+		outline: 0;
+		a {
+			font-size: 1.3rem;
+			white-space: nowrap;
+			color: white;
+		}
+	}
+`;
+const Button = styled.button`
+	background: ${props => props.theme.mediumBlue};
+	color: white;
+	padding: 0.5rem;
+	margin-left: 0.5rem;
+	cursor: pointer;
+	:focus {
+		outline: 0;
+	}
+`;
+const Wrapper = styled.div`
+	width: 100%;
+	padding: 1rem;
+`;
+const Div = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+function trimPost(post) {
+	var string = post;
+	var length = 165;
+	var trimmedString = string.substring(0, length) + '...';
+	return trimmedString;
+}
 class Search extends Component {
 	state = {
 		posts: [],
@@ -35,34 +91,32 @@ class Search extends Component {
 	};
 	render() {
 		return (
-			<div>
-				<div>
+			<Wrapper>
+				<SearchWrapper>
 					<input
 						type="text"
 						placeholder="Search"
 						onChange={e => this.setState({ searchString: e.target.value })}
 					/>
-					<button onClick={() => this._executeSearch()}>
+					<Button onClick={() => this._executeSearch()}>
 						<i class="fas fa-search fa-2x" />
-					</button>
-				</div>
+					</Button>
+				</SearchWrapper>
 				{this.state.posts.map((post, index) => (
 					<div>
-						<div>
-							<h2>{post.title}</h2>
-							<p>{post.content}</p>
-							<div>Posted By {post.author.name}</div>
-						</div>
-						<div>
+						<h2>{post.title}</h2>
+						<p>{trimPost(post.content)}</p>
+						<Div>
+							<p>Posted By {post.author.name}</p>
 							<div>
-								<button>
+								<PostButton>
 									<Link to={`/post/${post.id}`}>Read More</Link>
-								</button>
+								</PostButton>
 							</div>
-						</div>
+						</Div>
 					</div>
 				))}
-			</div>
+			</Wrapper>
 		);
 	}
 }
