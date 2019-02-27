57c27dc912ca51d778abc73f4e713c39a73bc156
Michael <checomichael2@gmail.com>
2019-02-27 12:19:52 -0500

    styling for create login/signup component (mobile)

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,7 +3,56 @@ import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
 import { AUTH_TOKEN } from '../constants';
 import styled from 'styled-components';
+const Wrapper = styled.div`
+	@media (min-width: 375px) {
+		display: column;
+		margin-top: 1rem;
+		width: 100%;
+	}
+`;
+const Button = styled.button`
+	@media (min-width: 375px) {
+		border: 1px solid ${props => props.theme.mediumBlue};
+		width: auto;
+		height: 30px;
+		border-radius: 5px;
+		margin-left: 0.5rem;
+		background: ${props => props.theme.mediumBlue};
+		/* position: relative;
+		top: 70%;
+		right: 90%; */
 
+		&:hover {
+			cursor: pointer;
+			opacity: 0.7;
+		}
+		outline: 0;
+		font-size: 1.3rem;
+		white-space: nowrap;
+		color: white;
+	}
+`;
+const ButtonWrapper = styled.div`
+	@media (min-width: 375px) {
+		display: flex;
+		justify-content: center;
+		margin-right: 1.5rem;
+	}
+`;
+const InputWrapper = styled.div`
+	@media (min-width: 375px) {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 1rem;
+		input {
+			width: 50%;
+			outline: 0;
+			margin: 0.5rem 0;
+			padding: 0.5rem;
+		}
+	}
+`;
 const SIGNUP_MUTATION = gql`
 	mutation signup($email: String!, $password: String!, $name: String!) {
 		signup(email: $email, password: $password, name: $name) {
@@ -43,24 +92,24 @@ class Login extends Component {
 	render() {
 		const { email, name, password, login } = this.state;
 		return (
-			<div>
-				<div>
+			<Wrapper>
+				<ButtonWrapper>
 					<Mutation
 						mutation={login ? LOGIN_MUTATION : SIGNUP_MUTATION}
 						variables={{ email, name, password }}
 						onCompleted={data => this._confirmData(data)}>
 						{mutation => (
-							<button onClick={mutation}>
+							<Button onClick={mutation}>
 								{login ? 'Login' : 'Create Account'}
-							</button>
+							</Button>
 						)}
 					</Mutation>
-					<button onClick={() => this.setState({ login: !login })}>
+					<Button onClick={() => this.setState({ login: !login })}>
 						{login ? 'Create an account' : 'Already have an account?'}
-					</button>
-				</div>
+					</Button>
+				</ButtonWrapper>
 
-				<div>
+				<InputWrapper>
 					{!login && (
 						<input
 							autoFocus
@@ -72,7 +121,6 @@ class Login extends Component {
 						/>
 					)}
 					<input
-						autoFocus
 						required
 						type="text"
 						name="email"
@@ -88,8 +136,8 @@ class Login extends Component {
 						placeholder="Password"
 						onChange={this.handleChange}
 					/>
-				</div>
-			</div>
+				</InputWrapper>
+			</Wrapper>
 		);
 	}
 }
